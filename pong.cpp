191eb7e58162454dc569941b486f0084fc87f72f
binarioGH <lacuentafalsadediegojonas@gmail.com>
2019-03-11 21:59:52 -0600

    paddles

---
+++ pong.cpp
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <windows.h>
+#include <conio.h>
+#include <cstdio>
+void gotoxy(int x, int y);
+void hideCursor(void);
+void drawBorders(void);
+
+class Paddle{
+private:
+	int x, y;
+	void impact(void);
+	void clean(void);
+	
+public:
+	Paddle(int _x, int _y); 
+    void randmove(void);
+    void drawPaddle(void);
+    void move(void); 
+    bool giveup = true;
+};
+
+Paddle::Paddle(int _x, int _y){
+	x = _x;
+	y = _y;
+}
+void Paddle::impact(void){
+	if(y+1>=22){
+		y -= 1;
+	}
+	if(y-1<=2){
+		y+= 1;
+	}
+	return;
+}
+void Paddle::clean(){
+	gotoxy(x,y-1);printf(" ");
+	gotoxy(x,y);printf(" ");
+	gotoxy(x,y+1);printf(" ");
+	return; 
+}
+void Paddle::drawPaddle(void){
+	gotoxy(x,y+1);printf("%c", 219);
+	gotoxy(x,y);printf("%c",219);
+	gotoxy(x,y-1);printf("%c", 219);
+	return; 
+}
+void Paddle::randmove(void){
+	Paddle::clean();
+	y += rand() % 2 - 1;
+	Paddle::impact();
+	Paddle::drawPaddle();
+	return;
+}
+void Paddle::move(void){
+
+	Paddle::clean();
+	if(kbhit()){
+		char key = getch();
+		switch(key){
+			case 'w':y-=1;break;
+			case 's':y+=1;break;
+			case 'q':giveup = true;break;
+		}
+	}
+	Paddle::impact();
+	Paddle::drawPaddle();
+	return;
+}
+class Ball{
+private:
+	int x, y;
+	int xd=1,yd=1;
+public:
+	Ball(int _x, int _y);
+	void changeDirection(int xc, int xy);
+};
+Ball::Ball(int _x, int _y){
+	x = _x;
+	y = _y;
+}
+void Ball::changeDirection(int xc, int xy){
+	if(xd == 1){
+		xd = -1;
+	}
+	else{
+		xd = 1;
+	}
+	if(yd == 1){
+		yd = -1;
+	}
+	else{
+		yd = 1;
+	}
+	return;
+}
+
+int main(){
+	system("cls");
+	hideCursor();
+	drawBorders();
+	Paddle player_paddle (4, 10);
+	Paddle rival_paddle (74, 10);
+	int ppoints = 0; //ppoints == player points;
+	int cpoints = 0; // cpoints == computer points
+	while(ppoints < 5 || cpoints < 5 || !player_paddle.giveup){
+		player_paddle.move();
+		rival_paddle.randmove();
+		Sleep(100);
+	}
+	return 0;
+}
+
+void gotoxy(int x, int y){
+	HANDLE hCon;
+	hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD dwPos;
+	dwPos.X = x;
+	dwPos.Y = y;
+	SetConsoleCursorPosition(hCon, dwPos);
+	return;
+}
+void hideCursor(void){
+	HANDLE hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO cci;
+	cci.dwSize = 0;
+	cci.bVisible = FALSE;
+	SetConsoleCursorInfo(hCon, &cci);
+	return;
+}
+void drawBorders(void){
+	int x =2;
+	int y =2;
+	for(x;x<=76;x++){
+		gotoxy(x,y);printf("%c",205);
+		y = 22;
+		gotoxy(x,y);printf("%c", 205);
+		y = 2;
+	}
+	x = 2;
+	for(y;y<=22;y++){
+		gotoxy(x,y);printf("%c", 186);
+		x = 77; 
+		gotoxy(x,y);printf("%c", 186);
+		x = 2;
+	}
+	gotoxy(2,2);printf("%c", 201);
+	gotoxy(77,2);printf("%c", 187);
+	gotoxy(2,22);printf("%c", 200);
+	gotoxy(77,22);printf("%c", 188);
+	return;
+}
